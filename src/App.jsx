cbc7707f4df74037a3a384803d1556a49b32837e
Andrea Huang <huangandrea@hotmail.it>
2025-04-24 15:43:06 +0200

    9. Practice Project :: adding the "New Project" component and reusable "Input" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
+    // flex -> allows to place children elements horizontally instead of stacking them vertically
     <main className="h-screen my-8 flex gap-8"> 
       <ProjectsSidebar/>
+      <NewProject/>
     </main>
   );
 }
